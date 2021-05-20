1a122c298b8c02f16a83408d2d01ab1ebebc5646
slowe <gravatar@strudel.org.uk>
2021-05-20 13:49:21 +0100

    Trim

--- resources/odi.log.js
+++ resources/odi.log.js
@@ -1,26 +1,24 @@
 /*
 	ODI Leeds Log
 	Web usage monitoring with less privacy invasion
-	Version: 2021-01-05
+	Version: 2021-05-19
 */
 (function(root){
 	if(!root.ODI) root.ODI = {};
+	var l = location;
 	function ok(t){
-		// If no array provided allow to pass
-		if(typeof t!=="object" || t.length <= 0) return true;
+		if(typeof t!=="object" || t.length <= 0) return true; // If no array provided allow to pass
 		for(i=0;i<t.length;i++){
-			// If the host matches allow to pass
-			if(t[i]==location.host) return true;
+			if(t[i]==l.host) return true; // If the host matches allow to pass
 		}
-		// You shall not pass!
-		return false;
+		return false; // You shall not pass!
 	}
 	function Log(){
 		var id,de,re,rg,sz,ua,tg;
 		de = "https://odileeds.org/log";
 		re = document.referrer;
-		rg = new RegExp(location.origin,"g");
-		if(re.indexOf(location.origin)==0) re = re.replace(rg,"");
+		rg = new RegExp(l.origin,"g");
+		if(re.indexOf(l.origin)==0) re = re.replace(rg,"");
 		sz = window.innerWidth+'x'+window.innerHeight;
 		this.setup = function(opt){
 			if(!opt) opt = {};
@@ -32,14 +30,14 @@
 		}
 		this.add = function(txt,cb){
 			// Don't log local file system
-			if(location.protocol=='file:') return this;
+			if(l.protocol=='file:') return this;
 			// If explicit targets set only allow them
 			if(!ok(tg)) return this;
-			var req = new XMLHttpRequest();
-			req.open('POST',de,true);
-			req.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8');
-			if(typeof cb==="function") req.onload = cb;
-			req.send((id ? 'oid='+id+'&':'')+'url='+location.href+'&'+(re ? 'ref='+re+'&':'')+(sz ? 'size='+sz+'&':'')+txt);
+			var r = new XMLHttpRequest();
+			r.open('POST',de,true);
+			r.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8');
+			if(typeof cb==="function") r.onload = cb;
+			r.send((id ? 'oid='+id+'&':'')+'url='+l.href+'&'+(re ? 'ref='+re+'&':'')+(sz ? 'size='+sz+'&':'')+txt);
 			return this;
 		}
 		return this;
